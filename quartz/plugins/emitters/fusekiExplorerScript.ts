5096acefe9cdc76966668ef5ffa588b4988641ef
wall121sa1 <153355619+wall121sa1@users.noreply.github.com>
2025-12-07 23:28:52 +0400

    Fix fuseki explorer prefix template

--- quartz/plugins/emitters/fusekiExplorerScript.ts
+++ quartz/plugins/emitters/fusekiExplorerScript.ts
@@ -19,7 +19,7 @@ const leafletLoader = () => {
   })
 }
 
-const buildPrefixes = (base) => \`PREFIX ci: <${base}>
+const buildPrefixes = (base) => \`PREFIX ci: <${'${'}base${'}'}>
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>
